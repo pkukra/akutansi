f18f8b58e457c55dc805d87755b1e572107686ba
EMR <emrpkukra@gmail.com>
2026-08-18 21:29:16 +0700

    re arrange menu

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -6,11 +6,18 @@ import {
     HomeOutlined,
     MonitorOutlined,
     PoweroffOutlined,
-    
+    ExperimentOutlined,
+    AuditOutlined,
+    ShoppingCartOutlined,
+    FormOutlined,
+    ReadOutlined,
+    CopyOutlined
 } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 
 const { Sider, Content, Footer } = Layout;
+
+import './AuthenticatedLayout.css'
 
 const items = [
     {
@@ -19,15 +26,70 @@ const items = [
         label: <a href={route("dashboard")}>Home</a>,
     },
     {
-        key: "akt-parent", // Untuk `/rm` dan turunannya
+        key: "keu-parent",
         icon: <LineChartOutlined />,
-        label: <a href={'#'}>Akutansi</a>,
+        label: <a href={'#'}>KEUANGAN</a>,
         children: [
             {
-                key: "akt.pendapatan_rajal.index",
-                label: (
-                    <a href={route("akt.pendapatan_rajal.index")}>Pendapatan Rajal</a>
-                ),
+                label: <a href={'#'}>Pendapatan</a>,
+                children: [
+                    {
+                        key: "keu-pendapatan-rajal",
+                        label: (
+                            <a href={route("akt.pendapatan_rajal.index")}>Lap Pendapatan Rajal</a>
+                        ),
+                    },
+                    {
+                        key: "keu-penerimaan-rajal",
+                        label: "Lap Penerimaan Rajal",
+                    },
+                    {
+                        key: "keu-pendapatan-ranap",
+                        label: "Lap Pendapatan Ranap",
+                    },
+                    {
+                        key: "keu_penerimaan-ranap",
+                        label: "Lap Penerimaan Ranap"
+                    },
+                    {
+                        key: "lap-pindah-buku",
+                        label: "Lap Pindah Buku"
+                    },
+                ]
+            },
+            {
+                key: "pengeluaran",
+                label: <a href={'#'}>pengeluaran</a>,
+                children: [
+                    {
+                        key: "pembayaran-kas",
+                        label: "Pembayaran Kas",
+                    },
+                    {
+                        key: "pembayaran-bank",
+                        label: "Pembayaran Bank",
+                    },
+                    {
+                        key: "pengeluaran-pindah-buku",
+                        label: "Pindah Buku",
+                    },
+                    {
+                        key: "lap-pengeluaran-kas",
+                        label: "Lap Pengeluaran Kas",
+                    },
+                    {
+                        key: "lap-pengeluaran-bank",
+                        label: "Lap Pengeluaran Bank",
+                    },
+                    {
+                        key: "lap-pengeluaran-kas-dan-bank",
+                        label: "Laporan pengeluaran Kas dan bank",
+                    },
+                    {
+                        key: "buku-pembantu-hutang",
+                        label: "Buku Pembantu Hutang",
+                    },
+                ]
             },
             {
                 key: "akt.pendapatan_rajal_jurnal_index.index",
@@ -35,15 +97,154 @@ const items = [
                     <a href={route("akt.pendapatan_rajal_jurnal_index.index")}>Jurnal Pendapatan Rajal</a>
                 ),
             },
-            
+
         ],
     },
     {
-        key: "casemix", // Untuk `/casemix` dan turunannya
-        icon: <MonitorOutlined />,
-        label: (
-            <a href={route("casemix.ranap-monit.list_pasien")}>Ranap Monitor</a>
-        ),
+        key: "farmasi",
+        label: <a href={'#'}>FARMASI</a>,
+        icon: <ExperimentOutlined />,
+        children: [
+            {
+                key: "input-pembelian",
+                label: "Input Pembelian",
+            },
+        ]
+    },
+    {
+        key: "pembelian",
+        label: "PEMBELIAN",
+        icon: <ShoppingCartOutlined />,
+        children: [
+            {
+                key: "lap-pembelian-rajal-obat-alkes",
+                label: "Lap Pembelian Obat dan Alkes Rajal",
+            },
+            {
+                key: "lap-pembelian-ranap-obat-alkes",
+                label: "Lap Pembelian Obat dan Alkes Ranap",
+            },
+            {
+                key: "lap-pembelian-logistik",
+                label: "Lap Pembelian Logistik",
+            },
+        ]
+    },
+    {
+        key: "hutang",
+        label: "HUTANG",
+        icon: <AuditOutlined />,
+        children: [
+            {
+                key: "daftar-hutang-by-umur-hutang",
+                label: "Daftar Hutang berdasarkan Jenis Hutang",
+            },
+            {
+                key: "daftar-umur-hutang",
+                label: "Daftar Umur Hutang",
+            },
+            {
+                key: "mutasi-daftar-hutang",
+                label: "Mutasi Daftar Hutang",
+            },
+            {
+                key: "buku-bantu-hutang",
+                label: "Buku Bantu Hutang",
+            },
+        ]
+    },
+    {
+        key: "jurnal",
+        icon: <FormOutlined />,
+        label: "JURNAL",
+        children: [
+            {
+                key: "Jurnal-Pendapatan-dan-Penerimaan-Rajal",
+                label: (
+                    <a href={route("akt.pendapatan_rajal_jurnal_index.index")}>Jurnal Pendapatan dan Penerimaan Rajal</a>
+                ),
+            },
+            {
+                key: "Jurnal-Pendapatan-dan-Penerimaan-Ranap",
+                label: "Jurnal Pendapatan dan Penerimaan Ranap",
+            },
+            {
+                key: "Jurnal-Pembelian-dan-Pengeluaran-Rawat-Jalan",
+                label: "Jurnal Pembelian dan Pengeluaran Rawat Jalan",
+            },
+            {
+                key: "Jurnal-Pembelian-dan-Pengeluaran-Rawat-Inap",
+                label: "Jurnal Pembelian dan Pengeluaran Rawat Inap",
+            },
+            {
+                key: "Jurnal-Umum",
+                label: "Jurnal Umum",
+                children: [
+                    {
+                        key: "Pindah-Buku-Kas-ke-bank",
+                        label: "Pindah Buku Kas ke bank",
+                    },
+                    {
+                        key: "Pindah-Buku-Bank-ke-Kas",
+                        label: "Pindah Buku Bank ke Kas",
+                    },
+                    {
+                        key: "Pindah-Buku-Bank-Ke-Bank",
+                        label: "Pindah Buku Bank Ke Bank",
+                    },
+                    {
+                        key: "Pindah-Buku-Kas-ke-Kas",
+                        label: "Pindah Buku Kas ke Kas",
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        key: "buku-bantu",
+        icon: <CopyOutlined />,
+        label: "BUKU BANTU",
+        children: [
+            {
+                key: "Buku-Pembantu-Piutang",
+                label: "Buku Pembantu Piutang",
+            },
+            {
+                key: "Buku-Pembantu-Hutang",
+                label: "Buku Pembantu Hutang",
+            },
+            {
+                key: "Buku-Pembantu-Persediaan",
+                label: "Buku Pembantu Persediaan",
+            },
+            {
+                key: "Daftar-Piutang",
+                label: "Daftar Piutang",
+            },
+        ]
+    },
+    {
+        key: "buku-besar",
+        icon: <ReadOutlined />,
+        label: "BUKU Besar",
+        children: [
+            {
+                key: "Sub-Sub-Buku-Besar",
+                label: "Sub Sub Buku Besar",
+            },
+            {
+                key: "Sub-Buku-Besar",
+                label: "Sub Buku Besar",
+            },
+            {
+                key: "Buku Besar",
+                label: "Buku Besar",
+            },
+            {
+                key: "Mutasi Buku Besar",
+                label: "Mutasi Buku Besar",
+            },
+        ]
     },
     {
         key: "profile",
@@ -82,6 +283,8 @@ const App = ({ children }) => {
                 collapsible
                 collapsed={collapsed}
                 onCollapse={handleCollapseChange}
+                width={250}
+                collapsedWidth={80}
             >
                 <div className="demo-logo-vertical" />
                 <Menu
--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,5 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::prefix('casemix')->group(function () {
-    Route::middleware('auth')->prefix('ranap-monit')->group(function () {
-        Route::get('/list_kamar_bangsal', [RanapMonitController::class, 'list_kamar_bangsal'])->name('casemix.ranap-monit.list_kamar_bangsal');
-
-        Route::get('/', [RanapMonitController::class, 'list_pasien'])->name('casemix.ranap-monit.list_pasien');
-        Route::get('/list_pasien_data', [RanapMonitController::class, 'list_pasien_data'])->name('casemix.ranap-monit.list_pasien_data');
-        Route::get('/list_billing_temp/{kode_reg}', [RanapMonitController::class, 'list_billing_temp'])->name('casemix.ranap-monit.list_billing_temp');
-
-        Route::get('/list_procedure/{kode_reg}', [RanapMonitController::class, 'list_procedure'])->name('casemix.ranap-monit.list_procedure');
-        Route::post('/save_procedure', [RanapMonitController::class, 'save_procedure'])->middleware(['auth', CheckRole::class . ':superadmin,koder'])->name('casemix.ranap-monit.save_procedure');
-        Route::delete('/delete_procedure/{id}', [RanapMonitController::class, 'delete_procedure'])->middleware(['auth', CheckRole::class . ':superadmin,koder'])->name('casemix.ranap-monit.delete_procedure');
-
-        Route::post('/update_monit_row/{kode_reg}', [RanapMonitController::class, 'update_monit_row'])->middleware(['auth'])->name('casemix.ranap-monit.update_monit_row');
-
-        Route::get('/get_list_cppt/{kode_reg}', [RanapMonitController::class, 'get_list_cppt'])->name('casemix.pasien-inap.get_list_cppt');
-
-        Route::get('/list_diagnosa/{kode_reg}', [RanapMonitController::class, 'list_diagnosa'])->name('casemix.ranap-monit.list_diagnosa');
-        Route::post('/save_diagnosa', [RanapMonitController::class, 'save_diagnosa'])->middleware(['auth', CheckRole::class . ':superadmin,koder'])->name('casemix.ranap-monit.save_diagnosa');
-        Route::delete('/delete_diagnosa/{id}', [RanapMonitController::class, 'delete_diagnosa'])->middleware(['auth', CheckRole::class . ':superadmin,koder'])->name('casemix.ranap-monit.delete_diagnosa');
-
-        Route::get('/download-xls', [RanapMonitController::class, 'download_pasien_data'])->name('casemix.ranap-monit.download_pasien_data_xls');
-    });
-});
-
 require_once __DIR__ . '/akutansi.php';
 require __DIR__ . '/auth.php';
